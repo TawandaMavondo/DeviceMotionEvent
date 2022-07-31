414a8d059d507f212979b061e10fa751032fa3b8
tawandamavondo <tawandagdmavondo@gmail.com>
2022-07-31 21:06:53 +0200

    .

--- app.js
+++ app.js
@@ -5,9 +5,39 @@ var vm2 = document.querySelector(".vm2")
 
 
 
-window.addEventListener('devicemotion', function (event) {
+// window.addEventListener('devicemotion', function (event) {
+//     vm.textContent = event.acceleration.x;
+//     vm1.textContent = event.acceleration.y;
+//     vm2.textContent = event.acceleration.z;
+//     console.log(`${event.acceleration.x} m/s2`);
+// });
+
+function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function executedFunction() {
+        var context = this;
+        var args = arguments;
+
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+};
+
+var returnedFunction = debounce(function (event) {
     vm.textContent = event.acceleration.x;
     vm1.textContent = event.acceleration.y;
     vm2.textContent = event.acceleration.z;
-    console.log(`${event.acceleration.x} m/s2`);
-});
+}, 350);
+
+window.addEventListener('devicemotion', returnedFunction);
