37b617c35f7716e3bbdf9bacc865a570eba8109b
tawandamavondo <tawandagdmavondo@gmail.com>
2022-08-01 15:32:13 +0200

    .

--- distance.js
+++ distance.js
@@ -1,8 +1,9 @@
 
-var vm = document.querySelector(".vm")
 var distance = 0;
 var thresholdAcceleration = 1;
+var hasDeviceMoved = false;
 
+// Debounce Function to delay the movement of the device 
 function debounce(func, wait, immediate) {
     var timeout;
 
@@ -27,52 +28,56 @@ function debounce(func, wait, immediate) {
 
 var motionEventHandler = debounce(function (event) {
     // Linear Accelaration on the x axis
-    // if (event.acceleration.x < thresholdAcceleration) {
-    //     return
-    // }
-    // vm.textContent = event.acceleration.x;
+    if (event.acceleration.x < thresholdAcceleration || hasDeviceMoved) {
+        return
+    }
+    hasDeviceMoved = true;
     getLocation();
 }, 750);
 
 
-var x = document.getElementById("demo");
+// Watch the current location of the device 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition, showError, { enableHighAccuracy: true, timeout: 1500 });
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        console.log("Geolocation is not supported by this browser.");
     }
 }
 
 // Previous Cordinates of the last taken picture
 var lat1 = -18.1881393;
 var lon1 = 31.5385135;
-var accuracy = document.querySelector(".accuracy");
+
+
 function showPosition(position) {
     // Current Position Cordinates 
     var lat2 = position.coords.latitude;
     var lon2 = position.coords.longitude;
     distance = getDistanceFromLatLon(lat2, lon2, lat1, lon1).toFixed(1).toString();
-    x.innerHTML = "Distance in Meters: " + getDistance() + " m";
-    vm.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-    accuracy.textContent = "Accuracy: " + Number(position.coords.accuracy).toFixed(2).toString() + " m";
+    console.log("Distance in Meters: " + getDistance() + " m");
+    console.log("Latitude: " + position.coords.latitude +
+        "\nLongitude: " + position.coords.longitude);
+    console.log("Accuracy: " + Number(position.coords.accuracy).toFixed(2).toString() + " m");
 }
+
 function getDistance() { return distance; }
+
+
 // Error function callback that shows if there is an error on getting current position
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
+            console.log("User denied the request for Geolocation.")
             break;
         case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
+            console.log("Location information is unavailable.");
             break;
         case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
+            console.log("The request to get user location timed out.");
             break;
         case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
+            console.log("An unknown error occurred.");
             break;
     }
 }
@@ -90,7 +95,7 @@ function getDistanceFromLatLon(lat1, lon1, lat2, lon2) {
         Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
     var d = R * c
-    return d*100
+    return d * 100
 }
 
 // Converts numeric degrees to radians
@@ -98,7 +103,6 @@ function toRad(Value) {
     return Value * Math.PI / 180
 }
 
-getLocation();
-
+window.addEventListener("devicemotion", motionEventHandler);
 
 
