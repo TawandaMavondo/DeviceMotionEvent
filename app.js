067b0b35acc3978764ba11c19fc0231b599eff8f
tawandamavondo <tawandagdmavondo@gmail.com>
2022-08-01 12:55:06 +0200

    .

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ function getLocation() {
 }
 
 // Previous Cordinates of the last taken picture
-var lat1 = 31.520007;
-var lon1 = -17.404954;
+var lat1 = -18.1881393;
+var lon1 = 31.5385135;
 var accuracy = document.querySelector(".accuracy");
 function showPosition(position) {
     // Current Position Cordinates 
@@ -80,21 +80,22 @@ function showError(error) {
 // distance in meters
 
 function getDistanceFromLatLon(lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-        ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;//Distance in M
+    var R = 6371 // km
+    var dLat = toRad(lat2 - lat1)
+    var dLon = toRad(lon2 - lon1)
+    var lat1 = toRad(lat1)
+    var lat2 = toRad(lat2)
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    var d = R * c
+    return d
 }
 
-function deg2rad(deg) {
-    return deg * (Math.PI / 180)
+// Converts numeric degrees to radians
+function toRad(Value) {
+    return Value * Math.PI / 180
 }
 
 getLocation();
